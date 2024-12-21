0045e3beb5fa1a08730f8eec45b166d2a589036e
Aldrinerd <aldrinjosepenaaa@gmail.com>
2024-12-21 13:09:30 +0800

    Delete LA/LA#2.py

--- LA/LA#2.py
+++
@@ -1,6 +0,0 @@
-class MLHero:
-    def __init__(self, hero_name, role):
-        self.hero_name = hero_name
-        self.role = role
-hero1 = MLHero("Layla","Marksman")
-print(hero1.hero_name, hero1.role)
