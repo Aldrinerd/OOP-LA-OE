865509eff892870984ce6207ac987d0494ef1eb6
Aldrinerd <aldrinjosepenaaa@gmail.com>
2024-12-21 13:14:36 +0800

    Create LA#3.py

---
+++ LA/3/LA#3.py
@@ -0,0 +1,8 @@
+class MLHero:
+    def __init__(self, hero_name, role):
+        self.hero_name = hero_name
+        self.role = role
+    def describe(self):
+        print(f"{self.hero_name} is a {self.role} hero.")
+hero1 = MLHero("Layla","marksman")
+hero1.describe()
