53f202ec5c8e6bf3ddaf0b5db21f62efcf0e8e05
Aldrinerd <aldrinjosepenaaa@gmail.com>
2024-12-22 15:12:38 +0800

    Create LA#28.py

---
+++ LA/28/LA#28.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+windows_ko = tk.Tk()
+name = "ALDRIN_PEÑA"
+windows_ko.title(f"OOP LA28 {name}")
+windows_ko.geometry("300x200")
+windows_ko.configure(bg = "white")
+windows_ko.mainloop()
